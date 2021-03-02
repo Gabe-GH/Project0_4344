e23e4e74c2abc55e44befa8c1d3d17b280977ae1
Gabriel Lira <gabriel.lira01@utrgv.edu>
2021-03-02 00:21:07 -0600

    file output need to pass test and work correctly

--- main.c
+++ main.c
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
         /* C2: -fFILENAME switch: open FILENAME and set it as the output
              stream */
         case 'f':
-          output = freopen((*(argv+1)),"w", stdout);
+          output = freopen((*argv)+2,"w", stdout);
           break;
         
         /* B3: fix the logical flow error in the switch*/
